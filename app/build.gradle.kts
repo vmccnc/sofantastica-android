2b88646ccb2b9ae1322d9aaa14982899b6997676
Dmitry Prokopenko <6558822@gmail.com>
2025-06-16 15:21:29 +0200

    Add Hilt navigation compose dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,4 +65,5 @@ dependencies {
     implementation(libs.retrofit)
     implementation(libs.retrofit.gson)
     implementation(libs.okhttp.logging)
+    implementation(libs.androidx.hilt.navigation.compose)
 }
